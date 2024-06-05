7f95ec5d7284fadbb32fdbc7265f58e3d7e56828
yahya <yahya.beka8@hotmail.com>
2024-06-05 09:29:26 +0100

    anapec to date

--- src/main/java/com/nttdata/CertificatesGenerator/controller/CandidateController.java
+++ src/main/java/com/nttdata/CertificatesGenerator/controller/CandidateController.java
@@ -1,28 +1,22 @@
 package com.nttdata.CertificatesGenerator.controller;
-
 import com.nttdata.CertificatesGenerator.entity.Candidate;
 import com.nttdata.CertificatesGenerator.exception.ResourceNotFoundException;
 import com.nttdata.CertificatesGenerator.repository.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-
 @RestController
 @RequestMapping("/api/candidate")
 @CrossOrigin("*")
 public class CandidateController {
-
     @Autowired
     private CandidateRepository candidateRepository; // we are passing service layer
-
 // get candidates list
 @GetMapping("/list")
 public List<Candidate> candidatesList(){
     return candidateRepository.findAll();
 }
-
 // find candidate by id
 @GetMapping ("/find/{candidateId}")
 public ResponseEntity<Candidate> getCandidateByID(@PathVariable Long candidateId) {
@@ -30,13 +24,11 @@ public ResponseEntity<Candidate> getCandidateByID(@PathVariable Long candidateId
             .orElseThrow(() -> new ResourceNotFoundException("Candidate with id -" + candidateId + "- not exist"));
     return ResponseEntity.ok(candidate);
 }
-
     // add candidate
     @PostMapping ("/add")
     public Candidate addCandidate (@RequestBody Candidate candidate){
         return candidateRepository.save(candidate);
     }
-
     // update candidate
     @PutMapping("update/{candidateId}")
     public ResponseEntity<Candidate>
@@ -51,17 +43,14 @@ public ResponseEntity<Candidate> getCandidateByID(@PathVariable Long candidateId
         candidate.setDiploma(candidateData.getDiploma());
         candidate.setGender(candidateData.getGender());
         candidate.setType(candidateData.getType());
-        candidate.setAnapec(candidateData.getAnapec());
+        candidate.setDate(candidateData.getDate());
         candidate.setPhoneNumber(candidateData.getPhoneNumber());
-
         Candidate updatedCandidate = candidateRepository.save(candidate);
         return ResponseEntity.ok(updatedCandidate);
     }
-
     // delete candidate
     @DeleteMapping("/delete/{candidateId}")
     public void deleteCandidate(@PathVariable Long candidateId) {
         candidateRepository.deleteById(candidateId);
     }
-
 }
